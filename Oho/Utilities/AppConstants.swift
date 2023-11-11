644fbcbb9a343167c7a6ed2fc5c05a8c5e7dcc43
sergpryimachuk <serg@pryimach.uk>
2023-11-11 16:20:51 +0100

    Add comment to the API key.

--- Oho/Utilities/AppConstants.swift
+++ Oho/Utilities/AppConstants.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 struct AppConstants {
+    // TODO: This API key is an example - register your won one at https://openweathermap.org.
     static let openWeatherAPIkey = "1eb817d31da30fe2ba6f85ea7d2aef54"
 }
